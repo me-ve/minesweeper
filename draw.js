dcccf4c5875c84b2caf4ccd233bd8c69abe67203
me-ve <68286584+me-ve@users.noreply.github.com>
2021-05-02 19:32:26 +0200

    Added lose condition

--- draw.js
+++ draw.js
@@ -4,6 +4,8 @@ let gridOffsetX, gridOffsetY;
 let colors;
 let timeStart, timeNow;
 let gameRunning;
+let gameEnded;
+let isWon;
 
 function draw(grid) {
     ctx.clearRect(0, 0, width, height);
@@ -16,31 +18,51 @@ function draw(grid) {
             let horStart = gridOffsetX + j * FIELD_WIDTH;
             ctx.strokeStyle = "#808080";
             if (gameRunning) {
-                ctx.fillStyle = grid[i][j].isMined ? "#FF00FF" : colors[grid[i][j].minesNearby];
                 switch (grid[i][j].status) {
                     case EXPOSED:
+                        ctx.fillStyle = grid[i][j].isMined ? "#000000" : colors[grid[i][j].minesNearby];
                         symbol = " ";
-                        if (grid[i][j].isMined) symbol = "#";
-                        else if (grid[i][j].minesNearby > 0) symbol = grid[i][j].minesNearby;
-                        ctx.fillText(symbol, horStart + FIELD_WIDTH / 4, verStart + FIELD_HEIGHT - 2)
+                        if (grid[i][j].isMined) {
+                            symbol = "☢";
+                            ctx.fillText(symbol, horStart + FIELD_WIDTH / 6, verStart + FIELD_HEIGHT - 2)
+                        } else if (grid[i][j].minesNearby > 0) {
+                            symbol = grid[i][j].minesNearby;
+                            ctx.fillText(symbol, horStart + FIELD_WIDTH / 4, verStart + FIELD_HEIGHT - 2);
+                        }
                         break;
                     case HIDDEN:
+                        ctx.fillStyle = "#808080";
+                        ctx.fillRect(horStart, verStart, FIELD_WIDTH, FIELD_HEIGHT);
                         break;
                     case FLAGGED:
+                        symbol = "⚑";
+                        ctx.fillStyle = "#FF0000";
+                        ctx.fillText(symbol, horStart + FIELD_WIDTH / 6, verStart + FIELD_HEIGHT - 2);
                         break;
                     case DETONATED:
+                        ctx.fillStyle = "#FF0000";
+                        symbol = "☢";
+                        ctx.fillText(symbol, horStart + FIELD_WIDTH / 6, verStart + FIELD_HEIGHT - 2);
                         break;
                     case MISTAKEN:
+                        symbol = "⛌";
                         break;
                 }
             }
             ctx.strokeRect(horStart, verStart, FIELD_WIDTH, FIELD_HEIGHT);
         }
     }
+    if (gameEnded) {
+        let message = isWon ? "You won." : "Game over.";
+        ctx.fillStyle = "#000000";
+        ctx.font = "48px Helvetica"
+        ctx.fillText(message, 24, 96);
+    }
     if (typeof time != "undefined") {
         ctx.fillStyle = "#000000";
         ctx.font = "48px Helvetica"
-        time.now = Date.now();
+        if (!gameEnded)
+            time.now = Date.now();
         let ms = time.now - time.start;
         let timeStr = ms / 1000 | 0;
         ctx.fillText(timeStr, 24, 48);
--- init.js
+++ init.js
@@ -3,7 +3,7 @@ function init() {
     ctx = canvas.getContext("2d");
     width = canvas.width;
     height = canvas.height;
-    mode = "easy";
+    mode = "hard";
     settings = config(mode);
     colors = [
         "#FFFFFF",
@@ -21,11 +21,13 @@ function init() {
     gridEndX = GRID_X_CENTER + (settings.fieldsHorizontal * FIELD_WIDTH) / 2;
     gridEndY = GRID_Y_CENTER + (settings.fieldsVertical * FIELD_HEIGHT) / 2;
     gameRunning = false;
+    gameEnded = false;
+    isWon = false;
     let grid;
     canvas.onclick = function(e) {
         let x = e.clientX - canvas.offsetLeft;
         let y = e.clientY - canvas.offsetTop;
-        if (between(x, gridOffsetX, gridEndX) && between(y, gridOffsetY, gridEndY)) {
+        if (between(x, gridOffsetX, gridEndX) && between(y, gridOffsetY, gridEndY) && !gameEnded) {
             let xIndex = (x - gridOffsetX) / FIELD_WIDTH | 0;
             let yIndex = (y - gridOffsetY) / FIELD_HEIGHT | 0;
             if (!gameRunning) {
@@ -35,7 +37,13 @@ function init() {
                 time = { start: Date.now() };
                 console.log("Game started.");
             } else {
-                console.log(grid[yIndex][xIndex]);
+                if (grid[yIndex][xIndex].isMined) {
+                    console.log("Mine detected.");
+                    grid[yIndex][xIndex].status = DETONATED;
+                    gameEnded = true;
+                } else {
+                    console.log("Field is clear.");
+                }
             }
         }
     }
